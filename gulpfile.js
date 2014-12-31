600839d757bdcd4bb1c0b8f1155d79434ba87947
Guillaume Morin <gmorin.pro@gmail.com>
2014-12-31 17:35:50 +0100

    add uglify gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,18 @@
 var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
+var uglify = require('gulp-uglify');
 
-gulp.task('webserver', function () {
-	nodemon({script: 'index.js', ext: 'html js'})
-	.on('restart', function () {
-		console.log('restarted!')
-	})
+gulp.task('minify', function() {
+  gulp.src('public/*.js')
+  .pipe(uglify())	
+  .pipe(gulp.dest('build/'));
 })
 
-gulp.task('default', ['webserver']);
+gulp.task('webserver', function () {
+  nodemon({script: 'index.js', ext: 'html js'})
+  .on('restart', function () {
+    console.log('restarted!')
+  })
+})
+
+gulp.task('default', ['minify', 'webserver']);
